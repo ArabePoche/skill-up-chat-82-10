7fb614463893823c8f59951e491c29d3b6ebfc0a
ArabePoche <arabedanslapoche@gmail.com>
2025-10-29 10:55:51 +0000

    Create index.ts

---
+++ src/streak/index.ts
@@ -0,0 +1,9 @@
+// Export des hooks
+export { useUserStreak } from './hooks/useUserStreak';
+export { useStreakConfig } from './hooks/useStreakConfig';
+export { useStreakTracker } from './hooks/useStreakTracker';
+
+// Export des composants
+export { StreakBadge } from './components/StreakBadge';
+export { StreakLevelsList } from './components/StreakLevelsList';
+export { StreakTrackerWrapper } from './components/StreakTrackerWrapper';
