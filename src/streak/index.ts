e56d19baf29a5a96cbdf315600c87b328bb53a34
ArabePoche <arabedanslapoche@gmail.com>
2025-10-29 14:11:01 +0000

    Update index.ts

--- src/streak/index.ts
+++ src/streak/index.ts
@@ -2,6 +2,8 @@
 export { useUserStreak } from './hooks/useUserStreak';
 export { useStreakConfig } from './hooks/useStreakConfig';
 export { useStreakTracker } from './hooks/useStreakTracker';
+export { useStreakSessionTracker } from './hooks/useStreakSessionTracker';
+export { useActivityTimeTracker } from './hooks/useActivityTimeTracker';
 
 // Export des composants
 export { StreakBadge } from './components/StreakBadge';
