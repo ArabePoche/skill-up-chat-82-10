6354f4af27649e34d77b0acd10105332065c554a
ArabePoche <arabedanslapoche@gmail.com>
2025-11-05 16:34:40 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'sonner';
 import PermissionManager from '@/components/PermissionManager';
 import useBackButtonHandler from '@/hooks/useBackButtonHandler';
 import { registerServiceWorker } from '@/offline/utils/registerSW';
+import LanguageOnboarding from '@/components/LanguageOnboarding';
 
 import Formation from '@/pages/Formation';
 import FormationDetail from '@/pages/FormationDetail';
@@ -97,6 +98,10 @@ const AppWithRouter: React.FC = () => {
 };
 
 const App: React.FC = () => {
+  const [showLanguageOnboarding, setShowLanguageOnboarding] = useState(
+    !localStorage.getItem('languageSelected')
+  );
+
   useEffect(() => {
     // Enregistrer le Service Worker au démarrage
     registerServiceWorker();
@@ -109,8 +114,14 @@ const App: React.FC = () => {
           <PresenceProvider>
             <NavigationProvider>
               <TabScrollProvider>
-                <AppWithRouter />
-                <Toaster />
+                {showLanguageOnboarding ? (
+                  <LanguageOnboarding onComplete={() => setShowLanguageOnboarding(false)} />
+                ) : (
+                  <>
+                    <AppWithRouter />
+                    <Toaster />
+                  </>
+                )}
               </TabScrollProvider>
             </NavigationProvider>
           </PresenceProvider>
