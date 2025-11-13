b232476d1b73d9db5c15f412a26af4527239f141
ArabePoche <arabedanslapoche@gmail.com>
2025-11-13 12:15:53 +0000

    Create School.tsx

---
+++ src/pages/School.tsx
@@ -0,0 +1,9 @@
+// Page principale du système de gestion scolaire
+import React from 'react';
+import { Desktop } from '@/school-os/components/Desktop';
+
+const School: React.FC = () => {
+  return <Desktop />;
+};
+
+export default School;
