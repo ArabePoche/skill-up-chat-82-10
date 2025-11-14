0a6f1a6edd0267271226cd2b6e3a0884bc2522de
ArabePoche <arabedanslapoche@gmail.com>
2025-11-14 19:39:03 +0000

    Update index.ts

--- src/school/components/index.ts
+++ src/school/components/index.ts
@@ -1,3 +1,4 @@
 // Export des composants de gestion des classes
 export { CreateClassModal } from './CreateClassModal';
+export { EditClassModal } from './EditClassModal';
 export { ClassesList } from './ClassesList';
