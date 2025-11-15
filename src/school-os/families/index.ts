25b257de3861387b9bf5298f1647f512e7e8655c
ArabePoche <arabedanslapoche@gmail.com>
2025-11-15 16:35:56 +0000

    Create index.ts

---
+++ src/school-os/families/index.ts
@@ -0,0 +1,6 @@
+// Export principal du module families
+export * from './hooks/useFamilies';
+export * from './components/FamilyManager';
+export * from './components/FamilyForm';
+export * from './components/StudentFamilySelector';
+export * from './components/FamilyFormSelector';
