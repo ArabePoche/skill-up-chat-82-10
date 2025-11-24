2807a00195eb4a94b00adaf55d620db154a405e2
ArabePoche <arabedanslapoche@gmail.com>
2025-11-24 20:30:13 +0000

    Update index.ts

--- src/school/hooks/index.ts
+++ src/school/hooks/index.ts
@@ -1,3 +1,7 @@
 // Export des hooks de gestion scolaire
 export * from './useSchool';
 export * from './useClasses';
+export * from './useTeachers';
+export * from './useSubjects';
+export * from './useClassSubjects';
+export * from './useClassTeachers';
