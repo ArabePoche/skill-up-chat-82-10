deb20d231fd37c24b0e55564c7e1119d325787c3
ArabePoche <arabedanslapoche@gmail.com>
2025-11-25 19:27:49 +0000

    Update index.ts

--- src/school/components/index.ts
+++ src/school/components/index.ts
@@ -3,3 +3,5 @@ export { CreateClassModal } from './CreateClassModal';
 export { CreateSubjectModal } from './CreateSubjectModal';
 export { EditClassModal } from './EditClassModal';
 export { ClassesList } from './ClassesList';
+export { default as SubjectEditDialog } from './SubjectEditDialog';
+export { default as SubjectFilesManager } from './SubjectFilesManager';
