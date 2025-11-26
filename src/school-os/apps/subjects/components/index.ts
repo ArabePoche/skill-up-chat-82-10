e4c417f63022e79b5a92ea18d19231fa65d19f61
ArabePoche <arabedanslapoche@gmail.com>
2025-11-26 06:51:20 +0000

    Create index.ts

---
+++ src/school-os/apps/subjects/components/index.ts
@@ -0,0 +1,4 @@
+// Export des composants pour la gestion des matières
+export { CreateSubjectDialog } from './CreateSubjectDialog';
+export { EditSubjectDialog } from './EditSubjectDialog';
+export { AssignSubjectsToClassDialog } from './AssignSubjectsToClassDialog';
