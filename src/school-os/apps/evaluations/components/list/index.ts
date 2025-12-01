0d351cf6450af778c417fdfeed01a7267112b697
ArabePoche <arabedanslapoche@gmail.com>
2025-12-01 20:05:49 +0000

    Create index.ts

---
+++ src/school-os/apps/evaluations/components/list/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Exports des composants de liste des évaluations
+ */
+export { ClassEvaluationCard } from './ClassEvaluationCard';
+export { ClassEvaluationStats } from './ClassEvaluationStats';
+export { EvaluationItem } from './EvaluationItem';
