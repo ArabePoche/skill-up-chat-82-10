a3bfbf223305f2d80961c3b826597a9da8b6ac68
ArabePoche <arabedanslapoche@gmail.com>
2025-12-07 12:49:46 +0000

    Create index.ts

---
+++ src/profile/components/tabs/school/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Index des composants du tab School
+ */
+export { default as SchoolLogo } from './SchoolLogo';
+export { default as MySchoolsCarousel } from './MySchoolsCarousel';
+export { default as SchoolSearchBar } from './SchoolSearchBar';
+export { default as SchoolSearchResults } from './SchoolSearchResults';
