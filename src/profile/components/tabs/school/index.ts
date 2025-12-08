5fbb15881e0dd7418ee867d823abeda50542970e
ArabePoche <arabedanslapoche@gmail.com>
2025-12-08 14:44:18 +0000

    Update index.ts

--- src/profile/components/tabs/school/index.ts
+++ src/profile/components/tabs/school/index.ts
@@ -5,3 +5,4 @@ export { default as SchoolLogo } from './SchoolLogo';
 export { default as MySchoolsCarousel } from './MySchoolsCarousel';
 export { default as SchoolSearchBar } from './SchoolSearchBar';
 export { default as SchoolSearchResults } from './SchoolSearchResults';
+export { default as SchoolWebPage } from './SchoolWebPage';
