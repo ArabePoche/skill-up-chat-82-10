59dba7008f2872e15d10e35757d8927380f1019c
ArabePoche <arabedanslapoche@gmail.com>
2026-01-01 21:43:10 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import App from "./App.tsx";
 import "./index.css";
 import "./styles/tiktok.css";
 import { initI18n } from "./i18n/config";
+import { registerServiceWorker } from "./offline/utils/registerSW";
 
 // Initialiser i18n après que React soit chargé
 initI18n();
+
+// Enregistrer le Service Worker pour le mode hors ligne
+registerServiceWorker();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
