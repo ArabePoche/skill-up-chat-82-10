73e7dbd8da21447e0d232096ffe12a24152cfea4
ArabePoche <arabedanslapoche@gmail.com>
2026-01-03 07:40:00 +0000

    Create index.ts

---
+++ src/local-storage/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Module de stockage local type WhatsApp
+ * Gestion offline-first des formations et conversations
+ */
+
+// Types
+export * from './types';
+
+// Stores
+export { messageStore } from './stores/MessageStore';
+export { formationStore } from './stores/FormationStore';
+
+// Hooks
+export { useLocalMessages } from './hooks/useLocalMessages';
+export { useLocalFormations, useLocalFormation } from './hooks/useLocalFormations';
+export { useStorageManager } from './hooks/useStorageManager';
