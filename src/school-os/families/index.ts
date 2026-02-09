85d5842da7f46bc846d884b323b79b7a85609351
ArabePoche <arabedanslapoche@gmail.com>
2026-02-09 22:19:07 +0000

    Update index.ts

--- src/school-os/families/index.ts
+++ src/school-os/families/index.ts
@@ -1,7 +1,11 @@
 // Export principal du module families
 export * from './hooks/useFamilies';
+export * from './hooks/useParentalCode';
 export * from './components/FamilyManager';
 export * from './components/FamilyForm';
 export * from './components/StudentFamilySelector';
 export * from './components/FamilyStudentsManager';
 export * from './components/FamilyFormSelectorDialog';
+export * from './components/ParentalCodeDisplay';
+export * from './components/ParentalCodeInput';
+export * from './components/ParentAssociationRequests';
