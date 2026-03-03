ba2172ad29e13ce9c1ad82ba33b917af038996cb
ArabePoche <arabedanslapoche@gmail.com>
2026-03-03 20:50:51 +0100

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-9dc17825'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/offline.html",
-    "revision": "0.cji38qukmto"
+    "revision": "0.1p5vjshh8fg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/offline.html"), {
